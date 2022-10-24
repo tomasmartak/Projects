7bfbbd35cf4f8f30e6a7343569eba3e56cf2028b
Tomáš Marťák <tomas.martak@gmail.com>
2022-10-24 13:04:13 +0200

    Rscript for MS analysis added
    Added, anonymised, made slightly less repulsive.

--- R/MS_visualiser.R
+++ R/MS_visualiser.R
@@ -1,135 +1,60 @@
-getwd()
-setwd("~/GitHub/Projects/R")
-getwd()
-### 2. Dataset Loading
-MSdata <-as_tibble("raw/MSdata.csv") #make formatting nice and usable
 ### 1. Libraries
 library(reshape2)
 library(dplyr)
 library(eulerr)
-#library(ggplot2)
+library(ggplot2)
 library(RVenn)
+
 ### 2. Dataset Loading
-MSdata <-as_tibble("raw/MSdata.csv") #make formatting nice and usable
-### 3. Filtering
-# Replace NaNs with 0. Replace anything under 21 (not including) with 0 --> data = irrelevant, as the intensity is too low.
-# Also removes all proteins where intensity = NaN/0/<21 across the board.
-MSdata[is.na(MSdata)] <- 0
-for (i in seq_along(MSdata[,1:5])) {
-MSdata[[i]][MSdata[[i]] <21] <- 0 #replace everything <21 with 0
-}
+MSdata <- as_tibble(read.csv2("raw/MSdata.csv", sep = ",")) #make formatting nice and usable
 #class(MSdata)
 #head(MSdata)
 #colnames(MSdata)
-View(MSdata)
-### 2. Dataset Loading
-MSdata <- read.csv2("raw/MSdata.csv")
-### 2. Dataset Loading
-MSdata <- read.csv2("raw/MSdata.csv", sep = ",")
-### 2. Dataset Loading
-MSdata <- as_tibble(read.csv2("raw/MSdata.csv", sep = ","))
-class(MSdata)
-head(MSdata)
-colnames(MSdata)
-View(MSdata)
-### 3. Filtering
+#View(MSdata)
+
+### 3. Filtering (based on the criteria that I received)
 # Replace NaNs with 0. Replace anything under 21 (not including) with 0 --> data = irrelevant, as the intensity is too low.
 # Also removes all proteins where intensity = NaN/0/<21 across the board.
-MSdata[is.na(MSdata)] <- 0
+MSdata[is.na(MSdata)] <- 0           
 for (i in seq_along(MSdata[,1:5])) {
-MSdata[[i]][MSdata[[i]] <21] <- 0 #replace everything <21 with 0
+  MSdata[[i]][MSdata[[i]] <21] <- 0 #replace everything <21 with 0
 }
-rm(i)
+rm(i) # I don't want i stored. Then again I don't know how else to remove it, and don't care to find out how for now, so here goes.
+
 MSdata <- MSdata[rowSums(MSdata[,1:5])>0,] #removes all proteins where values were 0/NaN
 MSdata <- MSdata[(MSdata$unique.pepts)>=2,] #removes all proteins where numbers of unique peptides measured > 2.
 for (i in seq_along(MSdata[,1:5])) {
-MSdata[[i]][MSdata[[i]] >=21] <- 1 #replace everything >=21 with 1; intensities are qualitative, not quantitative. We want a simple True/False/NA
+  MSdata[[i]][MSdata[[i]] >=21] <- 1 #replace everything >=21 with 1; intensities are qualitative, not quantitative. We want a simple True/False/NA
 }
-rm(i)
+rm(i) #ditto
+
 ### Remove anything after 1st semicolon in MSdata$name.gene/prot --> we want only 1 gene/protein name
 MSdata$name.gene <- gsub("\\;.*","",MSdata$name.gene)
 MSdata$name.prot <- gsub("\\;.*","",MSdata$name.prot)
-head(MSdata)
 #head(MSdata)
 #tidy up data into a basic useable dataframe
-MS.tidy <- MSdata[,c(8,3,4,5,1,2)] #orders in tidiest format; X= very probably positive control
-View(MS.tidy)
+MS.tidy <- MSdata[,c(8,3,4,5,1,2)] #orders in tidiest format based on protein name
 #View(MS.tidy)
 #MS.tidy <- dplyr::rename(MS.tidy, protein = name.prot)
 MS.tidy <- dplyr::rename(MS.tidy, protein = name.prot)
 MS.tidy <- as.data.frame(MS.tidy) #convert to dataframe only, as reshape can't handle tibbles; just a dplyr bug
-head(MS.tidy)
-### ***HEATMAP***
-#create new dataset for heatmap. Reorder so that gene is first.
-#class(MS.heat) #check whether it's a dataframe
-MS.heat <- reshape::melt(MS.tidy, id="protein") %>% dplyr::rename(sample = variable)
-#head(MS.heat)
-#View(MS.heat)
-##generate heatmap
-heatmap.total <- ggplot(MS.heat, aes(x = sample, y = protein, fill=value)) +
-geom_tile() +
-scale_fill_gradient(low = "white",
-high = "darkred",
-na.value = "grey70")+
-theme(axis.text.y = element_text(size = 5))
-library(ggplot2)
-### ***HEATMAP***
-#create new dataset for heatmap. Reorder so that gene is first.
-#class(MS.heat) #check whether it's a dataframe
-MS.heat <- reshape::melt(MS.tidy, id="protein") %>% dplyr::rename(sample = variable)
-#head(MS.heat)
-#View(MS.heat)
-##generate heatmap
-heatmap.total <- ggplot(MS.heat, aes(x = sample, y = protein, fill=value)) +
-geom_tile() +
-scale_fill_gradient(low = "white",
-high = "darkred",
-na.value = "grey70")+
-theme(axis.text.y = element_text(size = 5))
-heatmap.total
-head(MS.tidy)
-View(MS.tidy)
-### ***HEATMAP***
-#create new dataset for heatmap. Reorder so that gene is first.
-#class(MS.heat) #check whether it's a dataframe
-MS.heat <- reshape::melt(MS.tidy, id="protein") %>% dplyr::rename(sample = variable)
-head(MS.heat)
-View(MS.heat)
-#head(MS.heat)
-#View(MS.heat)
-##generate heatmap
-heatmap.total <- ggplot(MS.heat, aes(x = sample, y = protein, fill=value)) +
-geom_tile() +
-scale_fill_gradient(low = "white",
-high = "darkred",
-na.value = "grey70")+
-theme(axis.text.y = element_text(size = 5))
-heatmap.total
-### ***VENN DIAGRAM***
-#we start with MS.heat --> it's a good format; Protein names are irrelevant, so I removed them for MS.venn
-MS.venn = MS.tidy[,2:6] #can't include protein names --> this bit will have to be done manually
+#head(MS.tidy)
+#View(MS.tidy) #opens in new tab for ease of access
+
 ### ***VENN DIAGRAM***
 #we start with MS.heat --> it's a good format; Protein names are irrelevant, so I removed them for MS.venn
 MS.venn = MS.tidy[,2:6] #can't include protein names --> this bit will have to be done manually
 #head(MS.venn)
 MS.venn <- venn(MS.venn) #here you can use either venn or euler as a command; this will change the shape of the plot;
-#euler = based on size; venn = based on looking nice; also, can add shape = "ellipse" for more faithful
-#depiction (venn misses the X1/X4 only overlap, and solo X5)
-head(MS.tidy)
-#          panel.abline(v = 0, lty = 2)
-#          panel.dotplot(...)
-#        })
-#error_plot(MS.venn) #the dotplot and error plot show regions that are over (+)/under(-)represented in; based on the scale,
-# (0-1), these are of little concern, as we want information, not a perfect to-scale plot
-#MS.venn.ellipse <- euler(MS.venn, shape = "ellipse")
-#venneuler(MS.venn.ellipse)
-#error_plot(MS.venn.ellipse) #an ellipse shape gives practically no errors and is indeed the best representation
-#however, X2 fails to show up, which is bollocks, so I'm sticking to the sphere representation.
+                         #euler = based on size; venn = based on looking nice; also, can add shape = "ellipse" for more faithful 
+                         #depiction (venn misses the X1/X4 only overlap, and solo X5) 
+#head(MS.tidy)
 #MS.venn shows relevant data
 plot(MS.venn,
-quantities = T,
-lty = 1:3,
-labels = list(font = 4))
+     quantities = T,
+     lty = 1:3,
+     labels = list(font = 4))
+
 MS.venn.values <- MS.venn$original.values
 MS.venn.values
 print(MS.venn)
@@ -145,9 +70,14 @@ l4 <- list(subset(MS.mini, sample=="X4")$protein)
 l5 <- list(subset(MS.mini, sample=="X5")$protein)
 MS.list <- c(l1,l2,l3,l4,l5)
 names(MS.list) <- c("X1", "X2","X3","X4", "X5") #sets names for list; not strictly necessary, but makes indexing more legible
-summary(MS.list)
-###convert MS.list into an RVenn object and do the blasted combinations
+#summary(MS.list)
+
+### convert MS.list into an RVenn object and do the blasted combinations
+# Is there a better way to do this? Yes, there is. Can I quickly find it? No. Am I going to use this script again, though? Absolutely not.
+# Next time I'm actually going to follow a proper MS pipeline, so I'm not going to spend more time optimalising this disgusting stuff.
+# Why did I even do this? This was the output I was told to make. Do I personally think it is very useful? Nope.
 MS.RVenn <- Venn(MS.list)
+summary(MS.RVenn)
 a <- list(MS.list$X1[!(MS.list$X1 %in% RVenn::unite(MS.RVenn, c(2:5)))])
 b <- list(MS.list$X2[!(MS.list$X2 %in% RVenn::unite(MS.RVenn, c(1,3:5)))])
 c <- list(MS.list$X3[!(MS.list$X3 %in% RVenn::unite(MS.RVenn, c(1,2,4,5)))])
@@ -177,8 +107,11 @@ z <- list(setdiff(RVenn::overlap(MS.RVenn, c(3:5)), RVenn::unite(MS.RVenn, c(1,2
 alpha <- list(RVenn::overlap(MS.RVenn))
 Venn.summary <- as.list(c(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,alpha))
 names(Venn.summary) <- c(letters, "alpha") #set names for easier legibility
-View(Venn.summary)
-#nicely create a csv. file with the Venn.summary
+#View(Venn.summary)
+
+#nicely create a csv. file with the Venn.summary; this can be looked at along with the Venn diagram
 for(i in 1:27){
-write.table(Venn.summary[i], 'Annotation-proteins.csv', append= T, sep=',', row.names = TRUE)
+  write.table(Venn.summary[i], 'Annotation-proteins.csv', append= T, sep=',', row.names = TRUE)
 }
+
+
